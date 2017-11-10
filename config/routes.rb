ab43f6e3da9ef064f065d7513d7cc1ad66ebebbe
Jeremy Boom <jeremy.boom77@gmail.com>
2017-11-10 14:49:19 +0100

    Add figaro - Protect my secret data in config/application.yml

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 	root 'cocktails#index'
 	resources :doses, only: :destroy
-	resources :cocktails, only: [:index, :show, :new, :create] do 
+	resources :cocktails, only: [:index, :show, :new, :create] do
 		resources :doses, only: [:create, :new]
 	end
 end
